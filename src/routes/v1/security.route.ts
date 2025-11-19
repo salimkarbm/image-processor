d691a6831bbaefc9da7ff269bcb7713da59c9926
salim imuzai <salimimuzai@gmail.com>
2025-11-19 09:59:38 +0100

    feat:Get security recommendation - implement endpoint for fetching security recommendation info - write documentation for the security recommendation info [Delivered #Security/info]

--- src/routes/v1/security.route.ts
+++ src/routes/v1/security.route.ts
@@ -3,7 +3,7 @@ import { securityHeader, securityRecommendations } from '../../controllers';
 
 const router = Router();
 
-router.post('/security/info', securityRecommendations);
-router.post('/security/headers', securityHeader);
+router.get('/security/info', securityRecommendations);
+router.get('/security/headers', securityHeader);
 
 export default router;
